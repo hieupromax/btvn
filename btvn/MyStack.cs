1b815c050df72664549ad7d9e61ef1249b5ef5a4
hoang <hoang@LAPTOP-IIJV0RTO>
2025-05-16 20:59:06 +0700

    Add project files.

---
+++ btvn/MyStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btvn
+{
+    public class MyStack<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void Push(T item)
+        {
+            _items.Add(item);
+        }
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            T top = _items[_items.Count - 1];
+            _items.RemoveAt(_items.Count - 1);
+            return top;
+        }
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            return _items[_items.Count - 1];
+        }
+        public bool IsEmpty()
+        {
+            return _items.Count == 0;
+        }
+    }
+
+}
---
+++ btvn/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using btvn;
+
+MyStack<int> intStack = new MyStack<int>();
+intStack.Push(10);
+intStack.Push(20);
+Console.WriteLine("Top of intStack: " + intStack.Peek());
+Console.WriteLine("Popped from intStack: " + intStack.Pop());
+Console.WriteLine("intStack is empty? " + intStack.IsEmpty());
+
+Console.WriteLine();
+MyStack<string> stringStack = new MyStack<string>();
+stringStack.Push("Hello");
+stringStack.Push("World");
+Console.WriteLine("Top of stringStack: " + stringStack.Peek());
+Console.WriteLine("Popped from stringStack: " + stringStack.Pop());
+Console.WriteLine("stringStack is empty? " + stringStack.IsEmpty());
+
+Console.WriteLine();
+MyStack<double> doubleStack = new MyStack<double>();
+doubleStack.Push(1.1);
+doubleStack.Push(2.2);
+Console.WriteLine("Top of doubleStack: " + doubleStack.Peek());
+Console.WriteLine("Popped from doubleStack: " + doubleStack.Pop());
+Console.WriteLine("doubleStack is empty? " + doubleStack.IsEmpty());
